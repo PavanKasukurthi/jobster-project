15a20b1a751d6b4b4e994b510509dd6f4915f3f3
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2023-06-13 21:22:27 +0530

    Register/Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Landing, Error, Register, Dashboard } from './pages'
 
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 function App() {
   return (
     <Router>
@@ -10,6 +13,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="*" element={<Error />} />
       </Routes>
+      <ToastContainer position='top-center' />
     </Router>
   )
 }
