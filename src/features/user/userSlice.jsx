5bf50572150b7f2d69299b5d7e5efbeb29ece277
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2023-06-17 16:10:05 +0530

    login functionality

--- src/features/user/userSlice.jsx
+++ src/features/user/userSlice.jsx
@@ -24,7 +24,14 @@ export const registerUser = createAsyncThunk(
 export const loginUser = createAsyncThunk(
   'user/loginUser',
   async (user, thunkAPI) => {
-    console.log(`Login User: ${JSON.stringify(user)}`)
+    try {
+      const resp = await customFetch.post('/auth/login', user)
+      console.log(resp.data)
+      return resp.data
+    } catch (error) {
+      // console.log(error.response)
+      return thunkAPI.rejectWithValue(error.response.data.msg)
+    }
   }
 )
 
@@ -42,6 +49,19 @@ const userSlice = createSlice({
       toast.success(`Hello There ${user.name}`)
     },
     [registerUser.rejected]: (state) => {
+      state.isLoading = false
+      toast.error(payload)
+    },
+    [loginUser.pending]: (state) => {
+      state.isLoading = true
+    },
+    [loginUser.fulfilled]: (state, { payload }) => {
+      const { user } = payload
+      state.isLoading = false
+      state.user = user
+      toast.success(`Welcome back ${user.name}`)
+    },
+    [loginUser.rejected]: (state) => {
       state.isLoading = false
       toast.error(payload)
     },
